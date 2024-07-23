d0ef1d5a187fe6828b12944cfcc842168f7c0856
Andy Grundman <andy@hybridized.org>
2024-07-22 23:05:47 -0400

    Native CoreAudio renderer with spatial audio
    * Inspired by an example app from Apple. [1]
    * Needs an M1 or newer Mac.
    * Operates in a standard passthrough mode for stereo or when you have enough real channels (HDMI).
    * When headphones or built-in Macbook speakers are used and a 5.1 or 7.1 stream is being sent by
      Sunshine, this will render the surround channels into high quality spatial audio.
    * Supports optional head-tracking (enable in settings).
    * Supports personalized HRTF if you've scanned your ears with your iPhone. This can greatly improve spatial
      audio for many people.
    
    Known issues:
    * System sound menu does not indicate spatial audio is active or that multichannel audio is playing.
    * If Moonlight is in Game Mode and you toggle back and forth by say, swiping out of full screen to another app, the audio may stutter a bit.
    
    [1] https://developer.apple.com/documentation/audiotoolbox/generating_spatial_audio_from_a_multichannel_audio_stream

---
+++ app/streaming/audio/renderers/coreaudio/au_spatial_renderer.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "TPCircularBuffer.h"
+
+#include <AudioUnit/AudioUnit.h>
+#include <AudioToolbox/AudioToolbox.h>
+
+#include <Limelight.h>
+
+typedef void (^SimpleBlock)();
+
+class AUSpatialRenderer
+{
+public:
+    AUSpatialRenderer();
+    ~AUSpatialRenderer();
+    void cleanup();
+    void clearCallback();
+
+    double getAudioUnitLatency();
+    void setRingBufferPtr(const TPCircularBuffer* __nonnull buffer);
+    void setStatsTrackRenderBlock(SimpleBlock _Nonnull);
+    bool setup(AUSpatialMixerOutputType outputType, float sampleRate, int inChannelCount);
+    OSStatus setStreamFormatAndACL(float inSampleRate, AudioChannelLayoutTag inLayoutTag, AudioUnitScope inScope, AudioUnitElement inElement);
+    OSStatus setOutputType(AUSpatialMixerOutputType outputType);
+    OSStatus process(AudioBufferList* __nullable outputABL, AudioUnitRenderActionFlags* __nonnull ioActionFlags, const AudioTimeStamp* __nullable inTimestamp, uint32_t inNumberFrames);
+
+    friend OSStatus inputCallback(void * _Nonnull,
+                    AudioUnitRenderActionFlags *_Nullable,
+                    const AudioTimeStamp * _Nullable,
+                    uint32_t, uint32_t,
+                    AudioBufferList * _Nonnull);
+
+    uint32_t m_HeadTracking;
+    uint32_t m_PersonalizedHRTF;
+
+private:
+    AudioUnit _Nonnull m_Mixer;
+    const TPCircularBuffer* _Nonnull m_RingBufferPtr; // pointer to RingBuffer in the outer CoreAudioRenderer
+    SimpleBlock _Nonnull m_StatsTrackRenderBlock;
+
+    bool m_Initialized = false;
+    double m_AudioUnitLatency;
+};
