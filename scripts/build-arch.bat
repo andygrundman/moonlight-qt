6ce43808a7f1bb79ef28e8b516ddfce905395643
Andy Grundman <andy@hybridized.org>
2025-04-05 03:26:36 -0400

    test

--- scripts/build-arch.bat
+++ scripts/build-arch.bat
@@ -33,8 +33,6 @@ if /I "%BUILD_CONFIG%"=="debug" (
     )
 )
 
-echo Running with PATH=%PATH%
-
 rem Locate qmake and determine if we're using qmake.exe or qmake.bat
 rem qmake.bat is an ARM64 forwarder to the x64 version of qmake.exe
 where qmake.bat
